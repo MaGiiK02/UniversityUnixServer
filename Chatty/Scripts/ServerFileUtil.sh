867fe2c29e1951bca11c5b2b9bddf262c46f6633
A.Mattia <matti.pw@gmail.com>
2017-08-06 10:53:27 +0200

    Changed the bash script in order to remove the files older than t minutes

--- Chatty/Scripts/ServerFileUtil.sh
+++ Chatty/Scripts/ServerFileUtil.sh
@@ -2,7 +2,7 @@
 # Angelini Mattia 502688
 
 
-#CONSTATS -----------------------------
+#CONSTANTS -----------------------------
 usage="
 ${NC}
 The script provide a way to manage the files stored in the server,
@@ -12,21 +12,11 @@ to find where the server store it's media files.
 
 Script usage:
 
-  ./ServerFileUtil.sh [-h] [-b <base_path>] -t <0|1> config_file
+  ./ServerFileUtil.sh [-h] -t <int> config_file
 
 #########################################################################
 -h           : print this usage message.
--t <0|1>     : is the parameter that specify the type of the operation to
-               perform with the dir specified in the DirName field of the
-               provided file.
-               Operations :
-                  0.Print the path for all the server's media file in the
-                    DirName path of the server's config.
-                  1.Dellete all the file in DirName path of the server's
-                    config file.
--b <dirpath> : the base path from where apply the setting in the file,
-               if not provided, the script will assume the current path
-               as base path.
+-t <int>     : an integer that specify to the scrit h
 
 "
 
@@ -41,38 +31,29 @@ NC='\033[0m' # No Color
 
 # GLOBAL VARS --------------------
 
-Operation=-1
+Time=-1
 Filename=""
-BasePath="./"
 
 # END GRLOBAL VARS ---------------
 
 
 # Parsing the comand line
-while getopts "h:t:b:" arg; do
+while getopts "h:t:" arg; do
     case $arg in
       h) echo "$usage"
          exit 0
          ;;
 
-      t) Operation=$OPTARG
+      t) Time=$OPTARG
          ;;
 
-      b) BasePath="$OPTARG"
-         if [ ! -d $BasePath ]
-         then
-           printf "${RED}the provided base path is invalid!\n---->base_path_value=%s \n${NC}" "$BasePath">&2
-            exit 1
-         fi
-         ;;
-
-      :) printf "${RED}missing argument for -%s\n${NC}" "$OPTARG" >&2
-         echo "$usage" >&2
+      :) printf "${RED}missing argument for -%s\n${NC}" "$OPTARG"
+         echo "$usage"
          exit 1
          ;;
 
-      \?) printf "${RED}illegal option: -%s\n${NC}" "$OPTARG" >&2
-          echo "$usage" >&2
+      \?) printf "${RED}illegal option: -%s\n${NC}" "$OPTARG"
+          echo "$usage"
           exit 1
           ;;
     esac
@@ -80,17 +61,17 @@ done
 
 Filename="${@: -1}"
 
-if [ $Operation != 0 -a $Operation != 1 ]
+if [ $Time < 0 ]
 then
-    printf "${RED}invalid argument for -t!\n${NC}" >&2
-    echo "$usage" >&2
+    printf "${RED}%d is a invalid argument for -t\n${NC}"
+    echo "$usage"
     exit 1
 fi
 
 if [ -z $Filename ]
 then
-    printf "${RED}invalid file path\n${NC}" >&2
-    echo "$usage" >&2
+    printf "${RED}invalid file path\n${NC}"
+    echo "$usage"
     exit 1
 fi
 
@@ -100,14 +81,10 @@ printf "Start scanning :$Filename............"
 find=0
 setting_row=""
 while read -r line && [ $find -eq 0 ] ; do
-    if [[ "$line" =~ $dirname_setting_row_matcher ]]
+    if [[ "$line" == *""*$dirname_setting_row_matcher ]]
     then
        find=1
-    fi
-
-    if [ $find ]
-    then
-      setting_row=$line;
+       setting_row=$line;
     fi
 done < "$Filename"
 
@@ -115,29 +92,27 @@ echo "Ended!"
 
 if [ $find -eq 0 ]
 then
-    printf "${RED}error: The file don't contains a valid DirName field.\n${NC}" >&2
+    printf "${RED}error: The file don't contains a valid DirName field.\n${NC}"
     exit 1
 fi
 
 # value cutting
-dirname_path=${setting_row#*=}
-dirname_path=${dirname_path//[[:blank:]]/}
-dirname_path=$BasePath$dirname_path
+dirname_path=${setting_row#*=}#Removing the part before the equal
 
 if [ ! -d "$dirname_path" ]
 then
-  printf "${RED}error: The DirName setting have an invalid floder: \"%s\"\n${NC}" "$dirname_path">&2
-  echo "$usage" >&2
+  printf "${RED}error: The DirName setting have an invalid floder: \"%s\"\n${NC}" "$dirname_path"
+  echo "$usage"
   exit 1
 fi
 
-if [ $Operation -eq 0 ]
+if [ $Time -eq 0 ]
 then
-  echo "Performing Operation -t=0(Listing) for directory:$dirname_path."
+  echo "Performing listing operation for directory:$dirname_path."
   ls -l "$dirname_path"
 else
-  echo "Performing Operation -t=1(Cleanig) for directory:$dirname_path."
-  rm -r "$dirname_path\*"
+  echo "Performing cleaning operation for files older than $Time minutes for directory:$dirname_path."
+  find  $dirname_path -mmin +$Time -type f -delete
 fi
 
 echo "Operation performed with success!"
