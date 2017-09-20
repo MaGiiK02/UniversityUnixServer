d9ab84284045d189bc2eeb151c1d666bd155fe74
A.Mattia <matti.pw@gmail.com>
2017-09-20 18:02:58 +0200

    Added more Lock And Unlock Macro

--- Chatty/Sync/Sync.h
+++ Chatty/Sync/Sync.h
@@ -4,16 +4,28 @@
 #include "pthread.h"
 #include "stdio.h"
 
-#define LOCK_MUTEX(mutex) \
+#define LOCK_MUTEX_EXIT(mutex) \
   if(pthread_mutex_unlock(&mutex)!=0){ \
     perror("Error locking the mutex"); \
     exit(1); \
   }
 
-#define UNLOCK_MUTEX(mutex) \
+#define UNLOCK_MUTEX_EXIT(mutex) \
   if(pthread_mutex_unlock(&mutex)!=0){ \
     perror("Error unlocking the mutex"); \
     exit(1); \
+  }
+
+#define LOCK_MUTEX_RETURN(mutex,ret_val) \
+  if(pthread_mutex_unlock(&mutex)!=0){ \
+    perror("Error locking the mutex"); \
+    return ret_val; \
+  }
+
+#define UNLOCK_MUTEX_RETURN(mutex,ret_val) \
+  if(pthread_mutex_unlock(&mutex)!=0){ \
+    perror("Error unlocking the mutex"); \
+    return ret_val; \
   }
 
 #endif
