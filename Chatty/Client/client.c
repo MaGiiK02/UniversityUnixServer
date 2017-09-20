687988ce2f08619730267247806dc9b0eedd54fc
A.Mattia <matti.pw@gmail.com>
2017-09-20 18:01:56 +0200

    Fixed imports in the Client

--- Chatty/Client/client.c
+++ Chatty/Client/client.c
@@ -27,8 +27,8 @@
 #include <sys/mman.h>
 
 
-#include <connections.h>
-#include <ops.h>
+#include "../Connections/connections.h"
+#include "../Operations/ops.h"
 
 // tipo di operazione (usata internamente)
 typedef struct {
