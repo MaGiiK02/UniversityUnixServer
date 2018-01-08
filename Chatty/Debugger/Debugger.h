ac4f4cf9fa0e045e7d218c76060e260f63678320
A.Mattia <matti.pw@gmail.com>
2018-01-08 12:20:56 +0100

    Added some debug support function

--- Chatty/Debugger/Debugger.h
+++ Chatty/Debugger/Debugger.h
@@ -2,16 +2,17 @@
 // Created by Mattia Angelini on 23/11/2017.
 //
 
-#ifndef SOL_2017_LOGGER_H
-#define SOL_2017_LOGGER_H
+#ifndef SOL_2017_DEBUGGER_H
+#define SOL_2017_DEBUGGER_H
 
 #ifdef DEBUG
-# define Log(x) printf x
+#define Log(x) printf x
+#define ON_DEBUG(code) code
 #else
-# define Log(x) do {} while (0)
+#define Log(x) do {} while (0)
+#define ON_DEBUG(code) do{} while(0);
 #endif
 
-#define
 
 
-#endif //SOL_2017_LOGGER_H
+#endif //SOL_2017_DEBUGGER_H
