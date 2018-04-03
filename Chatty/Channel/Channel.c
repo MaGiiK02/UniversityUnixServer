c2564faef9fa00dc42ae0d5f1b62a240dfff4677
A.Mattia <matti.pw@gmail.com>
2018-04-03 15:06:22 +0200

    Fixed some mutex related bug

--- Chatty/Channel/Channel.c
+++ Chatty/Channel/Channel.c
@@ -2,95 +2,101 @@
 // Created by Mattia Angelini on 27/09/2017.
 //
 
+#include <string.h>
 #include "Channel.h"
 
-bool _isChEmpty(Ch* ch);
+bool _isChEmpty(Ch* ch){
+  return (ch->next == ch->next_insert) && ch->lastOp == OP_POP;
+}
 
-Ch* Ch_New(unsigned int size,ChFreeFunction freeFn){
+bool _isChFull(Ch* ch){
+  return ch->next_insert == ch->next && ch->lastOp == OP_PUSH;
+}
+
+Ch* Ch_New(unsigned int size, unsigned int elSize, ChFreeFunction freeFn){
   Ch* ch = malloc(sizeof(Ch));
-  ch->queue = malloc(sizeof(void)*size);
   int ris = pthread_mutex_init(&(ch->mu),NULL);
   if(ris != 0 ) exit(1);
-  ris = pthread_cond_init(&(ch->condWait),NULL);
+  ris = pthread_cond_init(&(ch->condWaitFull),NULL);
   if(ris != 0 ) exit(1);
+  ris = pthread_cond_init(&(ch->condWaitEmpty),NULL);
+  if(ris != 0 ) exit(1);
+  ch->elSize = elSize;
   ch->next_insert = 0;
   ch->next = 0;
   ch->size = size;
   ch->freeFn = freeFn;
   ch->closed = false;
+  ch->forcedClose = false;
+  ch->lastOp = OP_POP; //it's like the last el was removed
+  ch->queue = malloc(sizeof(void*)*size);
+  for(int i=0; i<size; i++){
+    ch->queue[i] = malloc(elSize);
+  }
   return ch;
 }
 
-void Ch_Close_S(Ch* ch){
-  LOCK_MUTEX_EXIT(ch->mu)
+void Ch_Close(Ch* ch,bool forceClose){
   ch->closed = true;
-  UNLOCK_MUTEX_EXIT(ch->mu)
+  ch->forcedClose = forceClose;
+  pthread_cond_broadcast(&(ch->condWaitEmpty));
 }
 
-void Ch_Free_S(Ch* ch){
-  LOCK_MUTEX_EXIT(ch->mu)
+void Ch_Free(Ch* ch){
   for(int i = 0; i<ch->size; i++){
     if(ch->queue[i]!= NULL){
       ch->freeFn(ch->queue[i]);
     }
   }
-  //ENSURE THREAD TO CLOSE {
-    ch->next_insert=ch->next_insert;
-    ch->closed = true;
-    UNLOCK_MUTEX_EXIT(ch->mu)
-    pthread_cond_broadcast(&(ch->condWait));
-    wait(10); // to be really sure about it
-    LOCK_MUTEX_EXIT(ch->mu)
-  //}
-  free(ch->queue);
-  UNLOCK_MUTEX_EXIT(ch->mu)
-  free(ch);
+  pthread_cond_destroy(&(ch->condWaitFull));
+  pthread_cond_destroy(&(ch->condWaitEmpty));
+  pthread_mutex_destroy(&(ch->mu));
+  for(int i=0; i<ch->size; i++){
+    ch->freeFn(ch->queue[i]);
+  }
+  FREE(ch->queue);
+  FREE(ch);
 
 }
 
 int Ch_Push_S(Ch* ch,void* el){
     LOCK_MUTEX_EXIT(ch->mu)
     if(!ch->closed){
-      ch->queue[ch->next_insert] = el;
+      while(_isChFull(ch)){
+        UNLOCK_MUTEX_EXIT(ch->mu);
+        pthread_cond_wait(&(ch->condWaitFull),&(ch->mu));
+      }
+      memcpy(ch->queue[ch->next_insert],el,ch->elSize);
       ch->next_insert = (ch->next_insert+1)%ch->size;
-      UNLOCK_MUTEX_EXIT(ch->condWait)
+      ch->lastOp=OP_PUSH;
+      pthread_cond_broadcast(&(ch->condWaitEmpty));
     }
     UNLOCK_MUTEX_EXIT(ch->mu)
+    return 0;
 }
 
-int  Ch_Pop_S(Ch* ch,void** out_el){
+int  Ch_Pop_S(Ch* ch,void* out_el){
   LOCK_MUTEX_EXIT(ch->mu)
-  if(_isChEmpty(ch)){
-    UNLOCK_MUTEX_EXIT(ch->mu)
-    UNLOCK_MUTEX_EXIT(ch->condWait)
-    LOCK_MUTEX_EXIT(ch->mu)
-  }
-  
+
   //IF empty the function wait for a push call on the same
   while(_isChEmpty(ch) && !ch->closed){
-    pthread_cond_wait(&(ch->condWait),&(ch->mu));
+    UNLOCK_MUTEX_EXIT(ch->mu)
+    pthread_cond_wait(&(ch->condWaitEmpty),&(ch->mu));
+    LOCK_MUTEX_EXIT(ch->mu)
+    //can happen that more thread are awakened at the same time
   }
-  if(_isChEmpty(ch) && ch->closed){
-      free(ch->queue);
+  if((_isChEmpty(ch) && ch->closed) || ch->forcedClose){
+      //if it's forced then: avoid to do anything and only notify all waiting threads tha the ch it's closed
       UNLOCK_MUTEX_EXIT(ch->mu)
-      free(ch);
+      pthread_cond_broadcast(&(ch->condWaitEmpty)); //the first thread to enter on close starts to to notify others waiting for the queue
       return 1;
   }
 
-  *out_el= ch->queue[ch->next];
-  ch->queue[ch->next] = NULL;
-  ch->next = (ch->next_insert+1)%ch->size;
+  memcpy(out_el,ch->queue[ch->next], sizeof(ch->elSize));// not perfect a copy functional approach it's way more correct
 
-  if(_isChEmpty(ch) && ch->closed){
-      free(ch->queue);
-    UNLOCK_MUTEX_EXIT(ch->mu)
-    free(ch);
-    return 1;
-  }
+  ch->next = (ch->next+1)%ch->size;
+  ch->lastOp = OP_POP;
+  pthread_cond_broadcast(&(ch->condWaitFull)); //tell to threads in write on socket that now can be empty and the filled
+  UNLOCK_MUTEX_EXIT(ch->mu)
   return 0;
-
-}
-
-bool _isChEmpty(Ch* ch){
-  return ch->next == ch->next_insert || ch->next == (ch->next_insert-1)%ch->size;
 }
--- Chatty/Channel/Channel.h
+++ Chatty/Channel/Channel.h
@@ -9,6 +9,10 @@
 #include <pthread.h>
 #include "../Sync/Sync.h"
 #include "../Booleans/Booleans.h"
+#include "../MemoryHelper/MemoryHelper.h"
+
+#define OP_PUSH 0
+#define OP_POP 1
 
 typedef void (*ChFreeFunction)(void *);
 
@@ -17,17 +21,21 @@ typedef struct {
     unsigned int next;
     unsigned int next_insert;
     unsigned int size;
+    unsigned int elSize;
+    int lastOp;
     bool closed;
+    bool forcedClose;
     pthread_mutex_t mu;
-    pthread_cond_t condWait;
+    pthread_cond_t condWaitFull;
+    pthread_cond_t condWaitEmpty;
     ChFreeFunction freeFn;
 } Ch;
 
-Ch* Ch_New(unsigned int size,ChFreeFunction freeFn);
-void Ch_Close_S(Ch* ch);
-void Ch_Free_S(Ch* cH);
+Ch* Ch_New(unsigned int size,unsigned int elSize,ChFreeFunction freeFn);
+void Ch_Close(Ch* ch,bool forceClose);
+void Ch_Free(Ch* cH);
 
 int Ch_Push_S(Ch* ch,void* el);
-int Ch_Pop_S(Ch* ch,void** out_el);
+int Ch_Pop_S(Ch* ch,void* out_el);
 
 #endif //_CHANNEL_H_
