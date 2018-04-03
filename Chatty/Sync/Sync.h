f6df3e90355324d66a6b998146215c54ca997904
A.Mattia <matti.pw@gmail.com>
2018-04-03 15:10:51 +0200

    Fix a bug in the Sync module

--- Chatty/Sync/Sync.h
+++ Chatty/Sync/Sync.h
@@ -6,25 +6,25 @@
 #include <stdio.h>
 
 #define LOCK_MUTEX_EXIT(mutex) \
-  if(pthread_mutex_unlock(&mutex)!=0){ \
+  if(pthread_mutex_unlock(&(mutex))!=0){ \
     perror("Error locking the mutex"); \
     exit(1); \
   }
 
 #define UNLOCK_MUTEX_EXIT(mutex) \
-  if(pthread_mutex_unlock(&mutex)!=0){ \
+  if(pthread_mutex_unlock(&(mutex))!=0){ \
     perror("Error unlocking the mutex"); \
     exit(1); \
   }
 
 #define LOCK_MUTEX_RETURN(mutex,ret_val) \
-  if(pthread_mutex_unlock(&mutex)!=0){ \
+  if(pthread_mutex_unlock(&(mutex))!=0){ \
     perror("Error locking the mutex"); \
     return ret_val; \
   }
 
 #define UNLOCK_MUTEX_RETURN(mutex,ret_val) \
-  if(pthread_mutex_unlock(&mutex)!=0){ \
+  if(pthread_mutex_unlock(&(mutex))!=0){ \
     perror("Error unlocking the mutex"); \
     return ret_val; \
   }
