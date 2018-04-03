3273fa3bbc0f14422726dc7191b3e5f42dca9907
A.Mattia <matti.pw@gmail.com>
2018-04-03 15:02:39 +0200

    Moved form the main floders

---
+++ Chatty/Server/ServerMessages/ServerMessages.c
@@ -0,0 +1,5 @@
+//
+// Created by Mattia Angelini on 03/04/2018.
+//
+
+#include "ServerMessages.h"
---
+++ Chatty/Server/ServerMessages/ServerMessages.h
@@ -0,0 +1,8 @@
+//
+// Created by Mattia Angelini on 03/04/2018.
+//
+
+#ifndef SOL_2017_SERVERMESSAGES_H
+#define SOL_2017_SERVERMESSAGES_H
+
+#endif //SOL_2017_SERVERMESSAGES_H
