29734402913ee0f6df925d70e05cf64bd9a1dbb1
A.Mattia <matti.pw@gmail.com>
2018-04-06 18:41:45 +0200

    RESET setress test

--- Chatty/Tests/teststress.sh
+++ Chatty/Tests/teststress.sh
@@ -39,7 +39,7 @@ for ((i=0;i<16;++i)); do
     ./client -l $1 -t 300 -k qui -S "aaaaaaaaaaaaaaaaaaaaaaaaaaa":pluto -S "bbbbbbbbbbbbbbbbb": -S "ccccccccccccccccc": -S "ddddddddddddddddddddd": -S "eeeeeeeeeeeeeeeeeeeee": -S "fffffffffffffffff": -S "gggggggggggggggd": -S "hhhhhhhhhhhhh": -S "iiiiiiiiiiiiiiiiiiiiii": -S "llllllllllllllllll": -p &
     ./client -l $1 -t 500 -k quo -L -p -S "aaaaaaaaaaaaaaaaaaaaaaaaaaa":pluto -S "bbbbbbbbbbbbbbbbb": -S "ccccccccccccccccc": -S "ddddddddddddddddddddd": -S "eeeeeeeeeeeeeeeeeeeee": -S "fffffffffffffffff": -S "gggggggggggggggd": -S "hhhhhhhhhhhhh": -S "iiiiiiiiiiiiiiiiiiiiii": -S "llllllllllllllllll": &
     ./client -l $1 -t 500 -k pippo -L -s chatty.o:qua -s client:qua -s libchatty.a:qua -p &
-    ./client -l $1 -t 200 -k qua -R 2 -s Config/chatty.conf1:pippo -S "aaaaaaaaaaaaaaaaaaaaaaaaaa":pippo -S "bbbbbbbbbbbbbbbbbb": -S "ccccccccccccccccc": -S "ddddddddddddddddddddd": -S "eeeeeeeeeeeeeeeeeeeee": -S "fffffffffffffffff": -S "gggggggggggggggd": -S "hhhhhhhhhhhhh": -S "iiiiiiiiiiiiiiiiiiiiii": -S "llllllllllllllllll": &
+    ./client -l $1 -t 200 -k qua -R 2 -s DATA/chatty.conf1:pippo -S "aaaaaaaaaaaaaaaaaaaaaaaaaa":pippo -S "bbbbbbbbbbbbbbbbbb": -S "ccccccccccccccccc": -S "ddddddddddddddddddddd": -S "eeeeeeeeeeeeeeeeeeeee": -S "fffffffffffffffff": -S "gggggggggggggggd": -S "hhhhhhhhhhhhh": -S "iiiiiiiiiiiiiiiiiiiiii": -S "llllllllllllllllll": &
     ./client -l $1 -t 100 -k minni -S "aaaaaaaaaaaaaaaaaaaaaaaaaaa":qua -S "bbbbbbbbbbbbbbbbb":pluto -S "ccccccccccccccccc": -S "ddddddddddddddddddddd":topolino -s ./libchatty.a:pluto -p &
     ./client -l $1 -t 300 -k "zio paperone" -S "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa":clarabella -S "bbbbbbbbbbbbbbbbbb": -S "ccccccccccccccccc": -S "ddddddddddddddddddddd":topolino -p &
     ./client -l $1 -t 100 -k clarabella -S "bbbbbbbbbbbbbbbbbbbbbbbbbbbbb": -R 1 -s chatty:minni -S "ccccccccccccccccc": -S "ddddddddddddddddddddd": -S "eeeeeeeeeeeeeeeeeeeee": -S "fffffffffffffffff": -S "gggggggggggggggd": -S "hhhhhhhhhhhhh": -S "iiiiiiiiiiiiiiiiiiiiii": -S "llllllllllllllllll": -p &
@@ -54,7 +54,7 @@ for ((i=0;i<16;++i)); do
     ./client -l $1 -c usente1
     ./client -l $1 -c usente2
     ./client -l $1 -k utente1 -R 5 -S connections.o:utente2 -p
-    ./client -l $1 -k utente2 -R 5 -p -S chatty.o:utente1 
+    ./client -l $1 -k utente2 -R 5 -p -S chatty.o:utente1
 
     ./client -l $i -C utente1
     ./client -l $i -C utente2
